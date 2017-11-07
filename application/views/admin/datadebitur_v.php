9660ca6ee4c08c687a5df57311cd8d864abdbe3a
rakahikmah <rakahikmah46@gmail.com>
2017-11-07 11:45:43 +0700

    tambah endtag endforeach endif

--- application/views/admin/datadebitur_v.php
+++ application/views/admin/datadebitur_v.php
@@ -13,7 +13,8 @@
         <div class="col-md-12">
           <div class="box">
             <div class="box-body">
-              <table id="datadebitur" class="table table-bordered table-hover">
+              <div class="table-responsive">
+              <table id="datadebitur" class="table  table-hover">
                 <thead>
                 <tr>
                   <th>Id Debitur</th>
@@ -32,10 +33,8 @@
                  
                 </thead>
                 <tbody>
-                   <?php 
-
-                    if($fetch_data->num_rows()>0){
-                      foreach ($fetch_data->result() as $row) {?>
+                   <?php if($fetch_data->num_rows()>0) :?> 
+                      <?php foreach ($fetch_data->result() as $row) :?>
                         <tr>
                           <td><?php echo $row->id_debitur;?></td>
                           <td><?php echo $row->username;?></td>
@@ -49,15 +48,12 @@
                           <td><?php echo $row->no_telp;?></td>
                           <td><?php echo $row->email;?></td>
                           <td><?php echo $row->jatuh_tempo;?></td>
-
-
                         </tr>
-                        
-                      <?php }
-
-                    }?>
+                      <?php endforeach; ?>
+                    <?php endif; ?>
                 </tbody>
               </table>
+             </div>
             </div>
             <!-- /.box-body -->
           </div>
