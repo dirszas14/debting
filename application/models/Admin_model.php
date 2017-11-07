67fd8e96c1db8ce9c4baa5f773089b2c3ef06e62
rakahikmah <rakahikmah46@gmail.com>
2017-11-07 11:38:04 +0700

    pindah coding insert dari controller ke model

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -1,32 +1,56 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Admin_model extends CI_Model{
+class Admin_model extends CI_Model {
 
-	function insert_data($data){
-		$this->db->insert("debitur",$data);
-	}
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	function fetch_data(){
-		$this->db->select("*");
-		$this->db->from("debitur");
-		$query=$this->db->get();
-		return $query;
-	}
-	function kode_otomatis(){
-            $this->db->select('Right(debting.id_debitur,2) as kode',false);
-            $this->db->order_by('id_debitur', 'desc');
+    public function insert_data_debitur(){
+        $data = array(
+            "id_debitur"    =>$this->input->post("iddebitur"),
+            "username"      =>$this->input->post("username"),
+            "password"      =>$this->input->post("notelp"),
+            "nama"          =>$this->input->post("nama"),
+            "nama_barang"   =>$this->input->post("nama_barang"),
+            "alamat"        =>$this->input->post("alamat"),
+            "harga_barang"  =>$this->input->post("harga_barang"),
+            "nik"           =>$this->input->post("nik"),
+            "jatuh_tempo"   =>$this->input->post("jatuh_tempo"),
+            "no_telp"       =>$this->input->post("notelp"),
+            "cicilan_min"   =>$this->input->post("cicilan_min"),
+            "email"         =>$this->input->post("email"),
+            "pekerjaan"     =>$this->input->post("pekerjaan")
+            );
+        $this->db->insert("debitur",$data);
+    }
+
+    public function fetch_data_debitur(){
+        $this->db->select("*");
+        $this->db->from("debitur");
+        $query=$this->db->get();
+        return $query;
+    }
+
+    public function getkodedebitur($table) { 
+            $this->db->select("RIGHT(debitur.id_debitur,3) AS kode ");
+            $this->db->order_by('id_debitur', 'DESC');
             $this->db->limit(1);
-            $query = $this->db->get('debting');
-            if($query->num_rows()<>0){
+            $query = $this->db->get('debitur');
+            if($query->num_rows()>0){
                 $data = $query->row();
                 $kode = intval($data->kode)+1;
             }else{
                 $kode = 1;
 
             }
-            $kodemax = str_pad($kode,2,"0",STR_PAD_LEFT);
-            $kodejadi  = "KD".$kodemax;
+            $kodemax = str_pad($kode,3,"0",STR_PAD_LEFT);
+            $kodejadi  = "DB".$kodemax;
             return $kodejadi;
+   } 
 
-        }
 }
+
+/* End of file Admin_model.php */
+/* Location: ./application/models/Admin_model.php */
