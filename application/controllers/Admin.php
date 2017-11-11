8e3a433fc67e8c242cbf08ddaccae06d02f54374
dirszas14 <31124351+dirszas14@users.noreply.github.com>
2017-11-11 22:53:01 +0700

    tambah dashboars special

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -18,6 +18,7 @@ class Admin extends CI_Controller {
 		$this->load->view('layout/header');
 		$this->load->view('layout/aside');
 		$this->load->view('admin/dashboard_v');
+		$this->load->view('admin/dashboardspecial_v');
 		$this->load->view('layout/footer');
 	}
 
