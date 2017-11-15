19136fee7b477bc48e947950b7c2ebadf2645b48
rakahikmah <rakahikmah46@gmail.com>
2017-11-15 19:51:17 +0700

    penambahan jquery

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -35,14 +35,52 @@
 	}
 </script>
     
+<script>
+	$('select[name=jumlah_angsuran]').change(function() {
+		var jumlah_angsuran = parseInt($(this).val());
+		var harga_barang = parseInt($('input[name=harga_barang]').val());
+		var tipe_pembayaran = parseInt($('select[name=tipe_pembayaran]').val());
+		var total_hargabarang;
+		var dp;
 
+		if (harga_barang <= 1000000){
+			total_hargabarang = parseInt(harga_barang)+(parseInt(harga_barang) * 0.05);
+				dp  		  = parseInt(total_hargabarang)*0.1;
+		}else if( harga_barang >= 1000000 && harga_barang <= 3000000){
+			total_hargabarang = parseInt(harga_barang)+(parseInt(harga_barang) * 0.07);
+				dp  			  = (parseInt(total_hargabarang)*0.1);
+		}else if(harga_barang >=3000000 && harga_barang <= 5000000){
+			total_hargabarang = parseInt(harga_barang)+(parseInt(harga_barang) * 0.08);
+				dp  			  = (parseInt(total_hargabarang)*0.1);
+		}else if(harga_barang >= 5000000){
+			total_hargabarang = parseInt(harga_barang)+(parseInt(harga_barang) * 0.09);
+				dp  			  = (parseInt(total_hargabarang)*0.1);
+		}
+
+		$('input[name=total_hargabarang]').val(parseInt(total_hargabarang));
+		$('input[name=bayar_perangsur]').val(parseInt((total_hargabarang-dp)/jumlah_angsuran));
+		$('input[name=bayar_dp]').val(parseInt(dp));
+		
+		var myDate=$('input[name=tanggal_pendaftaran]').val();
+		myDate=myDate.split("-");
+		var newDate=myDate[1]+"/"+myDate[0]+"/"+myDate[2];
+		var ubahtimestamp = new Date(newDate).getTime(); //will alert 1510592400000
+		var jangkawaktubayar =(tipe_pembayaran*jumlah_angsuran)*86400000;
+		var hitung = parseInt(ubahtimestamp)+jangkawaktubayar;
+		var t = new Date(hitung);
+		var format=t.toLocaleDateString();
+		tanggal_selesai_bayar=format.split("/");
+		var tgl_lunas=tanggal_selesai_bayar[0]+"-"+tanggal_selesai_bayar[1]+"-"+tanggal_selesai_bayar[2];
+		$('input[name=tanggal_selesai_bayar]').val(tgl_lunas);
+
+	});
+</script>
 
 <script>
 	$(function () {
 		$('#datadebitur').DataTable()
 		$('#kalender').fullCalendar()
 	})
-
 	// $('input[name=harga_barang]').on('input', function(e){        
 	//     $(this).val(formatCurrency(this.value.replace(/[,Rp.]/g,'')));
 	// }).on('keypress',function(e){
@@ -58,8 +96,6 @@
 	//     var n2 = n.replace(/\d\d\d(?!$)/g, "$&,");    
 	//     return 'Rp.' +n2.split('').reverse().join('');
 	// }
-
-
 </script>
 </body>
 </html>
